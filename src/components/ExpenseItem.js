c767df6d7578675cb62a4a02348da089340d999d
ErenDagli <daglieren7@gmail.com>
2022-09-12 20:04:54 +0300

    basic component added to project

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <h2>Expense Item!</h2>
+            <div>May 22th 2022</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$249.99</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
