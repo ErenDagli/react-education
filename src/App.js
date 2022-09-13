f42e7a71b474bd0cb397eea8b1dea7918f9253db
ErenDagli <daglieren7@gmail.com>
2022-09-13 22:54:43 +0300

    UI and Expenses Added to Project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
+import React from 'react'
 
-function App() {
+const App = () => {
 
   const expenses = [
     {
@@ -25,25 +26,15 @@ function App() {
     },
   ];
 
+  // return React.createElement('div', {},
+  //   React.createElement('h2', {}, "Welcome to My React Project"),
+  //   React.createElement(Expenses, { items: expenses }))
+
   return (
-    <div className="App">
+    <div>
       <h2>Welcome to My React Project</h2>
       <p>This is also visible</p>
-      <ExpenseItem title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date} />
-
-      <ExpenseItem title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date} />
-
-      <ExpenseItem title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date} />
-
-      <ExpenseItem title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date} />
+      <Expenses items={expenses} />
     </div>
   );
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,19 @@
-import "./ExpenseItem.css"
+import Card from "../UI/Card";
+import ExpenseDate from "../Expenses/ExpenseDate";
+
+import "../Expenses/ExpenseItem.css"
 
 function ExpenseItem(props) {
+
     return (
-        <div className="expense-item">
-            <div>{props.date.toDateString()}</div>
+        <Card className="expense-item">
+            <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-        </div>
-    )
+        </Card>
+    );
 }
 
 export default ExpenseItem;
